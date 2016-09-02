e68e512e3269f44fe0e8f7a1fce94dc58b9ca554
herno <russo.hernan@gmail.com>
2016-09-02 11:52:51 -0300

    Test Suite refactor. Add specs to the suite.

--- src/client/mainController.controller.spec.js
+++ src/client/mainController.controller.spec.js
@@ -1,46 +1,65 @@
 describe('Unit: MainController', function(){
-var vm;  
-  // var availableServerFactory = function($provide) {
-  //   $provide.factory('availableServer', ['$q', function($q){
-  //     var service = {
-  //       findServer: findServer
-  //     };
-  //     
-  //     return service;
-  //     
-  //     function findServer() {
-  //       if (passPromise) {
-  //         return $q.when('http://test-server.com');
-  //       } else {
-  //         return $q.reject('no available server');
-  //       }
-  //     }
-  //   }]);
-  // };
-  
-  beforeEach(module('myApp')); //function($provide) {
-  //   $provide.factory('availableServer', ['$q', function($q){
-  //     var findServer = jasmine.createSpy('findServer').and.CallFake(function () {
-  //       if (passPromise) {
-  //         return $q.when('http://test-server.com');
-  //       } else {
-  //         return $q.reject('no available server');
-  //       }
-  //     });
-  //     return {
-  //       findServer: findServer
-  //     };
-  //   }]);
-  // }));
-  beforeEach(inject( function($controller){
-    vm = $controller('MainController');
+
+  var vm, availableServerMock, passPromise, scope,
+    successMsg = 'http://test-server.com',
+    rejectMsg = 'No available server';
+
+  // Mocking availableServer service
+  beforeEach(function(){
+    module(function($provide){
+      $provide.factory('availableServerMock', function($q){
+        var service = {
+          findServer: findServer
+        };
+        return service;
+
+        // mock pblic method
+        function findServer () {
+          if (passPromise) {
+            return $q.when(successMsg);
+          } else {
+            return $q.reject(rejectMsg);
+          }
+        }
+      });
+    });
+    module('myApp');
+  });
+
+  // instance MainController
+  beforeEach( inject( function($rootScope, $controller, _availableServerMock_){
+    scope = $rootScope.$new();
+    availableServerMock = _availableServerMock_;
+
+    // keep track of service activity
+    spyOn(availableServerMock, 'findServer').and.callThrough();
+    vm = $controller('MainController',{
+      availableServer: availableServerMock,
+      $scope: scope
+    });
   }));
-  
-  it('should have vm.findServer function defined', function () {
-      // mainController.findServer();
-      // expect(availableServer.findServer).toHaveBeenCalled();
-      expect(vm.findServer).toBeDefined();
-      
-      // expect(ctrl.server).toEqual('http://testServer.com');
+
+  //spec suite
+  it('should have findServer function defined', function () {
+    expect(vm.findServer).toBeDefined();
+  });
+
+  it('and should call findServer method on availableServer service when it\'s invoked ', function(){
+    vm.findServer();
+    expect(availableServerMock.findServer).toHaveBeenCalled();
+  });
+
+  it('and should get a server name as response', function(){
+    passPromise = true;
+    vm.findServer();
+    scope.$digest();
+    expect(vm.server).toEqual(successMsg);
+  });
+
+  it('or should get a rejection message', function(){
+    passPromise = false;
+    vm.findServer();
+    scope.$digest();
+    expect(vm.server).toEqual(rejectMsg);
   });
 });
